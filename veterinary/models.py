33c29abd5d74c31b0156c5604905b7909384737b
Matthew2720 <mhernandezrpo22@gmail.com>
2022-11-01 14:20:49 -0500

    Migracion de modelo

--- veterinary/models.py
+++ veterinary/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+
+# Modelos MER.
+class Veterinary(models.Model):
+    nameVeterinary = models.CharField(max_length=50,blank=False,null=False)
+    cityVeterinary = models.CharField(max_length=50,blank=True,null=True)
+    nit = models.CharField(max_length=50,blank=False,null=False)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.nameVeterinary
+    
+#UserModel
+class User(AbstractUser):
+    direccion = models.CharField(max_length=50,blank=False, null= False)
+    veterinary = models.ForeignKey(Veterinary, on_delete=models.PROTECT,null=True,blank=True)
+    
+
+
+#EndUserModel
+    
+class Client(models.Model):
+    veterinary = models.ForeignKey(Veterinary, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50,blank=False,null=False)
+    last_name = models.CharField(max_length=50,blank=False,null=False)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15,blank=True,null=True)
+    mobil = models.CharField(max_length=15,blank=False,null=False)
+    
+    def __str__(self):
+        return self.name
+    
+class Pet(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    namePet = models.CharField(max_length=30,blank=False,null=False)
+    species = models.CharField(max_length=30,blank=True,null=True)
+    age = models.CharField(max_length=3 , blank=False, null = False)
+    
+    def __str__(self):
+        return self.namePet
+
