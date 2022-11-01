cd192c6f5439a987e6da9cb1c55af427ef342a66
Matthew2720 <mhernandezrpo22@gmail.com>
2022-11-01 15:50:53 -0500

    Modularizacion de los templates

--- PetsFiles/settings.py
+++ PetsFiles/settings.py
@@ -79,6 +79,14 @@ DATABASES = {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
+    # 'default': {  
+    #     'ENGINE': 'django.db.backends.mysql',  
+    #     'NAME': 'petsfiles',  
+    #     'USER': 'root',  
+    #     'PASSWORD': 'Mateohr96',  
+    #     'HOST': '127.0.0.1',  
+    #     'PORT': '3306',  
+    # }  
 }
 
 
--- veterinary/views.py
+++ veterinary/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'veterinary/index.html',{})
+
+def prueba(request):
+    return render(request,'veterinary/index2.html',{})
