f10311201cf16fec5b1fca7b3b9dc631ff13db62
Matthew2720 <mhernandezrpo22@gmail.com>
2022-11-01 14:27:48 -0500

    Modelo migrado, parece funcional

--- veterinary/admin.py
+++ veterinary/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Veterinary,Client,Pet,User
+
+admin.site.register(User)
+admin.site.register(Veterinary)
+admin.site.register(Client)
+admin.site.register(Pet)
+
 
 # Register your models here.
