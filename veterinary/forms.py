44888447c89704be843efc227cd1da3e30b39f99
matthew2720 <mhernandezrpo22@gmail.com>
2023-03-08 04:48:10 -0500

    Cambios actuales

--- veterinary/forms.py
+++ veterinary/forms.py
@@ -1,92 +1,96 @@
 from django.forms import ModelForm, TextInput, EmailInput, Select, PasswordInput, CharField, DecimalField, \
     inlineformset_factory
-from django.forms import HiddenInput,DateInput,DateTimeInput,NumberInput,ChoiceField,Form
+from django.forms import HiddenInput, DateInput, DateTimeInput, NumberInput, ChoiceField, Form
 from xdg.Exceptions import ValidationError
 
 from .models import *
 
+
 class DateTimePickerInput(DateTimeInput):
-        input_type = 'datetime-local'
+    input_type = 'datetime-local'
+
+
 class VeterinaryForm(ModelForm):
     class Meta:
         model = Veterinary
         fields = '__all__'
         widgets = {
-            'nameVeterinary': TextInput(attrs={'class':'form-control'}),
-            'cityVeterinary': TextInput(attrs={'class':'form-control'}),
-            'nit': TextInput(attrs={'class':'form-control'}) ,
-            'email': TextInput(attrs={'class':'form-control'}),
-            'direccion': TextInput(attrs={'class':'form-control'}),
-            'password':PasswordInput(attrs={'class':'form-control'}),
+            'nameVeterinary': TextInput(attrs={'class': 'form-control'}),
+            'cityVeterinary': TextInput(attrs={'class': 'form-control'}),
+            'nit': TextInput(attrs={'class': 'form-control'}),
+            'email': TextInput(attrs={'class': 'form-control'}),
+            'direccion': TextInput(attrs={'class': 'form-control'}),
+            'password': PasswordInput(attrs={'class': 'form-control'}),
         }
         labels = {
             'nameVeterinary': 'Veterinaria ',
             'cityVeterinary': 'Ciudad ',
             'nit': 'Nit ',
-            'email':'Email ',
-            'direccion':'Direccion',
-            'password':'Contraseña '
+            'email': 'Email ',
+            'direccion': 'Direccion',
+            'password': 'Contraseña '
         }
+
 
 class UserForm(ModelForm):
-
     class Meta:
         model = User
-        fields = ('username','first_name','last_name','password','direccion','email','groups')
+        fields = ('username', 'first_name', 'last_name', 'password', 'direccion', 'email', 'groups')
         help_texts = {
-            'username':None,'first_name':None,'last_name':None,'password':None,
-            'direccion':None,'email':None,'groups':None
+            'username': None, 'first_name': None, 'last_name': None, 'password': None,
+            'direccion': None, 'email': None, 'groups': None
         }
         widgets = {
-            'username': TextInput(attrs={'class':'form-control'}),
-            'first_name': TextInput(attrs={'class':'form-control'}),
-            'last_name': TextInput(attrs={'class':'form-control'}) ,
-            'password': PasswordInput(attrs={'class':'form-control'}) ,
-            'direccion': TextInput(attrs={'class':'form-control'}) ,
-            'email': EmailInput(attrs={'class':'form-control'}),
-            'groups': Select(attrs={'class':'form-control'}),
+            'username': TextInput(attrs={'class': 'form-control'}),
+            'first_name': TextInput(attrs={'class': 'form-control'}),
+            'last_name': TextInput(attrs={'class': 'form-control'}),
+            'password': PasswordInput(attrs={'class': 'form-control'}),
+            'direccion': TextInput(attrs={'class': 'form-control'}),
+            'email': EmailInput(attrs={'class': 'form-control'}),
+            'groups': Select(attrs={'class': 'form-control'}),
         }
+
 
 class UserFormWithoutPassword(UserForm):
     class Meta:
         model = User
-        fields = ('username','first_name','last_name','direccion','email')
+        fields = ('username', 'first_name', 'last_name', 'direccion', 'email')
         help_texts = {
-            'username':None,'first_name':None,'last_name':None,'password':None,
-            'direccion':None,'email':None,'groups':None
+            'username': None, 'first_name': None, 'last_name': None, 'password': None,
+            'direccion': None, 'email': None, 'groups': None
         }
         widgets = {
-            'username': TextInput(attrs={'class':'form-control'}),
-            'first_name': TextInput(attrs={'class':'form-control'}),
-            'last_name': TextInput(attrs={'class':'form-control'}) ,
-            'password': PasswordInput(attrs={'class':'form-control'}) ,
-            'direccion': TextInput(attrs={'class':'form-control'}) ,
-            'email': EmailInput(attrs={'class':'form-control'}),
-            'groups': Select(attrs={'class':'form-control'}),
+            'username': TextInput(attrs={'class': 'form-control'}),
+            'first_name': TextInput(attrs={'class': 'form-control'}),
+            'last_name': TextInput(attrs={'class': 'form-control'}),
+            'password': PasswordInput(attrs={'class': 'form-control'}),
+            'direccion': TextInput(attrs={'class': 'form-control'}),
+            'email': EmailInput(attrs={'class': 'form-control'}),
+            'groups': Select(attrs={'class': 'form-control'}),
         }
 
 
 class ClientForm(ModelForm):
-
     class Meta:
         model = Client
         fields = '__all__'
         widgets = {
-            'veterinary': HiddenInput(attrs={'class':'form-control form-input'}),
-            'name':TextInput(attrs={'class':'form-control form-input'}),
-            'last_name':TextInput(attrs={'class':'form-control form-input'}),
-            'document':TextInput(attrs={'class':'form-control form-input'}),
-            'email':EmailInput(attrs={'class':'form-control form-input'}),
-            'phone':TextInput(attrs={'class':'form-control form-input'}),
+            'veterinary': HiddenInput(attrs={'class': 'form-control form-input'}),
+            'name': TextInput(attrs={'class': 'form-control form-input'}),
+            'last_name': TextInput(attrs={'class': 'form-control form-input'}),
+            'document': TextInput(attrs={'class': 'form-control form-input'}),
+            'email': EmailInput(attrs={'class': 'form-control form-input'}),
+            'phone': TextInput(attrs={'class': 'form-control form-input'}),
         }
         labels = {
             'veterinary': 'Veterinaria',
-            'name':'Nombres',
-            'last_name':'Apellidos',
-            'email':'Email',
-            'phone':'Telefono',
-            'document':'Cedula'
+            'name': 'Nombres',
+            'last_name': 'Apellidos',
+            'email': 'Email',
+            'phone': 'Telefono',
+            'document': 'Cedula'
         }
+
 
 class PetForm(ModelForm):
     def __init__(self, VeterinaryLogued, *args, **kwargs):
@@ -96,28 +100,29 @@ class PetForm(ModelForm):
     class Meta:
         model = Pet
         fields = '__all__'
-        CHOICES = (('Macho', 'Macho'),('Hembra', 'Hembra'),)
-        CHOICESPECIES = (('Felino', 'Felino'),('Canino', 'Canino'),('Aves','Aves'),('Otro','Otro'))
+        CHOICES = (('Macho', 'Macho'), ('Hembra', 'Hembra'),)
+        CHOICESPECIES = (('Felino', 'Felino'), ('Canino', 'Canino'), ('Aves', 'Aves'), ('Otro', 'Otro'))
         widgets = {
-            'client': Select(attrs={'class':'form-control'}),
-            'namePet':TextInput(attrs={'class':'form-control'}),
-            'species':Select(attrs={'class':'form-control'},choices= CHOICESPECIES),
-            'gender':Select(attrs={'class':'form-control'},choices= CHOICES ),
-            'birthdate':DateInput(attrs={'class':'form-control','type':'date'}),
+            'client': Select(attrs={'class': 'form-control'}),
+            'namePet': TextInput(attrs={'class': 'form-control'}),
+            'species': Select(attrs={'class': 'form-control'}, choices=CHOICESPECIES),
+            'gender': Select(attrs={'class': 'form-control'}, choices=CHOICES),
+            'birthdate': DateInput(attrs={'class': 'form-control', 'type': 'date'}),
         }
         labels = {
             'client': 'Cliente',
-            'namePet':'Nombre de la Mascota',
-            'species':'Especie',
-            'gender':'Genero',
-            'birthdate':'Fecha de nacimiento'
+            'namePet': 'Nombre de la Mascota',
+            'species': 'Especie',
+            'gender': 'Genero',
+            'birthdate': 'Fecha de nacimiento'
         }
 
 
 class EventForm(ModelForm):
     def __init__(self, VeterinaryLogued, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['pet'].queryset = Pet.objects.select_related('client__veterinary').filter(client__veterinary=VeterinaryLogued)
+        self.fields['pet'].queryset = Pet.objects.select_related('client__veterinary').filter(
+            client__veterinary=VeterinaryLogued)
         self.fields['doctor'].queryset = User.objects.filter(veterinary=VeterinaryLogued, is_doctor=True)
 
     class Meta:
@@ -155,19 +160,20 @@ class EventForm(ModelForm):
             'name': 'Tipo de servicio'
         }
 
+
 class CategoryForm(ModelForm):
     class Meta:
         model = Category
         fields = '__all__'
         widgets = {
-            'name':TextInput(attrs={'class':'form-control'}),
-            'desc':TextInput(attrs={'class':'form-control'})
+            'name': TextInput(attrs={'class': 'form-control'}),
+            'desc': TextInput(attrs={'class': 'form-control'})
         }
         labels = {
             'name': 'Nombre',
             'desc': 'Descripcion'
         }
-        
+
 
 class OrderForm(Form):
     ORDER_CHOICES = (
@@ -180,8 +186,8 @@ class OrderForm(Form):
         ('pvp', 'Precio Ascendente'),
         ('-pvp', 'Precio Descendente'),
     )
-    
-    order_by = ChoiceField(choices=ORDER_CHOICES, required=False,label='Ordenar por')
+
+    order_by = ChoiceField(choices=ORDER_CHOICES, required=False, label='Ordenar por')
 
 
 class ProductForm(ModelForm):
@@ -203,14 +209,11 @@ class ProductForm(ModelForm):
 
 
 class SaleForm(ModelForm):
-    client_document = CharField(max_length=20, required=True, label='Cliente (documento)', widget=TextInput(attrs={'class': 'form-control', 'autocomplete': 'off'}))
-    sub_total = DecimalField(max_digits=8, decimal_places=2, required=True, label='Subtotal', widget=NumberInput(attrs={'class': 'form-control', 'step': '0.01'}))
-    iva = DecimalField(max_digits=8, decimal_places=2, required=True, label='IVA', widget=NumberInput(attrs={'class': 'form-control', 'step': '0.01'}))
-    total = DecimalField(max_digits=8, decimal_places=2, required=True, label='Total', widget=NumberInput(attrs={'class': 'form-control', 'step': '0.01', 'readonly': False}))
-
+    client_document = CharField(max_length=20, required=True, label='Cliente (documento)',
+                                widget=TextInput(attrs={'class': 'form-control', 'autocomplete': 'off'}))
     class Meta:
         model = Sale
-        exclude = ['client']
+        exclude = ['client', 'created_by', 'total']
 
     def clean_client_document(self):
         client_document = self.cleaned_data.get('client_document')
@@ -228,15 +231,21 @@ class SaleForm(ModelForm):
         return instance
 
 
-
 class DetSaleForm(ModelForm):
+    iva = DecimalField(max_digits=8, decimal_places=2, label='IVA',
+                       widget=NumberInput(attrs={'class': 'form-control', 'step': '0.01' , 'name': 'iva'}))
+    subtotal = DecimalField(max_digits=10, decimal_places=2, label='Subtotal',
+                            widget=NumberInput(attrs={'class': 'form-control', 'readonly': True, 'name': 'subtotal'}))
+    pvp = DecimalField(max_digits=10, decimal_places=2, label='Precio',
+                       widget=NumberInput(attrs={'class': 'form-control', 'readonly': True, 'name': 'pvp'}))
+
     class Meta:
         model = DetSale
-        fields = ['product', 'quantity', 'price']
+        fields = ['product', 'quantity']
+
         widgets = {
-            'product': Select(attrs={'class': 'form-control'}),
-            'quantity': NumberInput(attrs={'class': 'form-control'}),
-            'price': NumberInput(attrs={'class': 'form-control'}),
+            'product': Select(attrs={'class': 'form-control', 'onChange': 'update_price()', 'name': 'product'}),
+            'quantity': NumberInput(attrs={'class': 'form-control', 'min': 1 , 'name': 'quantity'}),
         }
 
     def __init__(self, *args, **kwargs):
@@ -244,4 +253,4 @@ class DetSaleForm(ModelForm):
         self.fields['product'].queryset = Product.objects.all()
 
 
-SaleFormSet = inlineformset_factory(Sale, DetSale, form=DetSaleForm, extra=1)
+SaleFormSet = inlineformset_factory(Sale, DetSale, form=DetSaleForm, extra=8)
--- veterinary/models.py
+++ veterinary/models.py
@@ -1,5 +1,6 @@
 import uuid
 from datetime import datetime
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.forms import model_to_dict
@@ -127,8 +128,8 @@ class Product(models.Model):
 
 class Sale(models.Model):
     created_date = models.DateTimeField(auto_now_add=True, null=True)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE,default=0)
-    client = models.ForeignKey(Client, on_delete=models.CASCADE,default=0)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, default=0)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, default=0)
     total = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
 
 
